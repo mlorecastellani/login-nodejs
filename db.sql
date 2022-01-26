323495e780edb6a9dbeb220392d26d243170015f
mlorecastellani <locastellani@gmail.com>
2022-01-26 18:14:15 -0300

    sql

--- db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `heroku_bb5004d1cb5227f`.`users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `user` VARCHAR(45) NULL,
-  `name` VARCHAR(100) NULL,
-  `rol` VARCHAR(45) NULL,
-  `pass` VARCHAR(255) NULL,
-  PRIMARY KEY (`id`));
-
-//
